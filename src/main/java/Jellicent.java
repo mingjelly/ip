1019ad039c239c9f701cd94ae8e96726d7e4879f
Yap Ming Yang <yap.mingyang02@gmail.com>
2026-01-23 21:56:31 +0800

    Add command list functionality

--- src/main/java/Jellicent.java
+++ src/main/java/Jellicent.java
@@ -8,9 +8,21 @@ public class Jellicent {
         Scanner scanner = new Scanner(System.in);
         System.out.println("----------------------------------------------------------------------");
         String command = scanner.nextLine();
+
+        String[] commands = new String[100];
+        int counter = 0;
+
         while (!command.equals("bye")) {
             System.out.println("----------------------------------------------------------------------");
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i=0; i < counter; i++) {
+                    System.out.println(i+1 + ". " + commands[i]);
+                }
+            } else {
+                commands[counter] = command;
+                System.out.println("added: " + command);
+                counter++;
+            }
             System.out.println("----------------------------------------------------------------------");
             command = scanner.nextLine();
 
@@ -20,4 +32,6 @@ public class Jellicent {
         System.out.println("----------------------------------------------------------------------");
 
     }
+
+
 }
