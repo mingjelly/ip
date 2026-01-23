628d038e3d41601804346d36a17e0e80e1e8d9ac
Yap Ming Yang <yap.mingyang02@gmail.com>
2026-01-23 15:49:52 +0800

    "Completed Level 1"

--- src/main/java/Jellicent.java
+++ src/main/java/Jellicent.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Jellicent {
     public static void main(String[] args) {
         String name = "Jellicent";
+        System.out.println("----------------------------------------------------------------------");
         System.out.println("Hello from " + name + "\nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----------------------------------------------------------------------");
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println("----------------------------------------------------------------------");
+            System.out.println(command);
+            System.out.println("----------------------------------------------------------------------");
+            command = scanner.nextLine();
+
+        }
+        System.out.println("----------------------------------------------------------------------");
         System.out.println("Bye! Hope to see you again!");
+        System.out.println("----------------------------------------------------------------------");
+
     }
 }
