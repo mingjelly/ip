fc1a575e0b2c402c0b40ee5f3f9c25e6046a6433
Yap Ming Yang <yap.mingyang02@gmail.com>
2026-01-23 14:56:07 +0800

    "Completed Level 0"

---
+++ src/main/java/Jellicent.java
@@ -0,0 +1,7 @@
+public class Jellicent {
+    public static void main(String[] args) {
+        String name = "Jellicent";
+        System.out.println("Hello from " + name + "\nWhat can I do for you?");
+        System.out.println("Bye! Hope to see you again!");
+    }
+}
