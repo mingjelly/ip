d33d1046c58e41bcdae7c24bcbddd8d03b395630
Yap Ming Yang <yap.mingyang02@gmail.com>
2026-02-03 23:52:10 +0800

    Fix bugs relating to displayPicture

--- src/main/java/jellicent/ui/DialogBox.java
+++ src/main/java/jellicent/ui/DialogBox.java
@@ -22,6 +22,8 @@ import jellicent.command.CommandType;
 public class DialogBox extends HBox {
     @FXML
     private Label dialog;
+    @FXML
+    private ImageView displayPicture;
 
     public DialogBox(String text, Image img) {
         try {
